f7438bdd3ccdae62cef5031f19d1ad35179d3263
Alejo <alemarchan@gmail.com>
2022-11-15 19:30:47 -0300

    Se dividió en funciones los elementos repetitivos de mensajes de error

--- index.py
+++ index.py
@@ -3,7 +3,8 @@ import database as db
 
 def despliega_menu(menu):
     """
-    Toma como parámetro el diccionario menu y hace un print de sus elementos
+    Toma como parámetro el diccionario (menu) y hace un print de sus elementos
+    para que sean visulizadas las opciones correspondientes
     """
     for key, values in menu.items():
         print("({}) para \"{}\"".format(key, values))
@@ -20,36 +21,57 @@ def valida_numero(elemento):
 
 def valida_eleccion(diccionario, elemento):
     """
-    Toma como parámetros el diccionario del menú y la seleccion del usuario
+    Toma como parámetros el diccionario (menu) y la seleccion del usuario (elemento)
     y devuelve como resultado True si el elemento pertenece a la clave del menú
     """
-    print(type(elemento))
     if elemento in diccionario:
+        os.system('cls')
         return True
     else:
         os.system('cls')
         return False
 
+def solicita_eleccion():
+    """
+    Devuelve como resultado el input del usuario
+    """
+    eleccion = input("Ingrese su eleccion: ")
+    return eleccion
+
+def imprime_respuesta(flag):
+    """
+    Funcion que devuelve al usuario los comentarios de su elección si es errónea
+    Toma como parámetro una cadena (flag) y dependiendo, del error, genera el print
+    """
+    if flag == 'no_numero':
+        print("No elegiste un número. Ingrese valor numérico!!!")
+    elif flag == 'no_eleccion':
+        print("Tu elección no existe en este listado. Ingrese valor correspondiente")
+    else:
+        print("No tenemos respuesta para esto :D")
+
+
 
 def menu_general():
     """
     El menú general es invocado al inicio del programa.
-    El mismo despliega los items principales a mostrar al administrador para su elección
+    El mismo despliega los items principales a mostrar al usuario para su elección
     """
-    
+    print("***** Menú General *****")
     menu = {
         1: "Menú Libros",
         2: "Menú Usuarios",
         3: "Menú Préstamos",
         0: "Salir"
     }
+    #Pasa por parámetro el diccionario con los elementos del respectivo Menú
     despliega_menu(menu)
 
     eleccion = input("Ingrese su eleccion: ")
 
     if not valida_numero(eleccion):
         os.system('cls')
-        print("Eleccion errónea. Ingrese valor numérico correspondiente")
+        imprime_respuesta('no_numero')
         menu_general()
     else:
         ele_conv = int(eleccion)
@@ -57,38 +79,79 @@ def menu_general():
             print("Eleccion errónea. Ingrese valor correspondiente")
             menu_general()
         else:
-            print("Eleccion correcta {}".format(ele_conv))
+            #print("Eleccion correcta {}".format(ele_conv))
             if ele_conv == 1:
+                os.system('cls')
                 menu_libros()
             if ele_conv == 2:
+                os.system('cls')
                 menu_usuarios()
             if ele_conv == 3:
+                os.system('cls')
                 menu_prestamo()
             else:
                 exit
 
 def menu_libros():
-    os.system('cls')
+    print("***** Menú Libros *****")
     menu = {
         1: "Libros Disponibles",
         2: "Libros En Préstamo",
-        3: "Nuevo Libro",
+        3: "Ingreso Nuevo Libro",
         4: "Actualizar Libro",
-        5: "Baja Libro"
+        5: "Baja Libro",
+        6: "Volver al Menú Principañ"
     }
     despliega_menu(menu)
-    eleccion = input("Ingrese su eleccion: ")
+    eleccion = solicita_eleccion()
     if not valida_numero(eleccion):
         os.system('cls')
-        print("Eleccion errónea. Ingrese valor numérico correspondiente")
+        imprime_respuesta('no_numero')
         menu_libros()
     else:
         ele_conv = int(eleccion)
         if not valida_eleccion(menu,ele_conv):
-            print("Eleccion errónea. Ingrese valor correspondiente")
+            imprime_respuesta('no_eleccion')
             menu_libros()
         else:
-            print("Eleccion correcta {}".format(ele_conv))
+            #print("Eleccion correcta {}".format(ele_conv))
+            if ele_conv == 1:
+                print("libros disponibles")
+            if ele_conv == 2:
+                print("libros en préstamo")
+            if ele_conv == 3:
+                print("Ingreso Nuevo libro")
+            if ele_conv == 4:
+                print("Actualizar libro")
+            if ele_conv == 5:
+                print("Dar Baja de libro")
+            if ele_conv == 6:
+                menu_general()
+            else:
+                exit
+    
+def menu_usuarios():
+    print("***** Menú Usuarios *****")
+    menu = {
+        1: "Alta Cliente",
+        2: "Consulta Cliente",
+        3: "Actualizar Cliente",
+        4: "Eliminar Cliente",
+        5: "Volver al Menú Principal"
+    }
+    despliega_menu(menu)
+    eleccion = solicita_eleccion()
+    if not valida_numero(eleccion):
+        os.system('cls')
+        imprime_respuesta('no_numero')
+        menu_usuarios()
+    else:
+        ele_conv = int(eleccion)
+        if not valida_eleccion(menu,ele_conv):
+            imprime_respuesta('no_eleccion')
+            menu_usuarios()
+        else:
+            #print("Eleccion correcta {}".format(ele_conv))
             if ele_conv == 1:
                 pass
             if ele_conv == 2:
@@ -98,66 +161,36 @@ def menu_libros():
             if ele_conv == 4:
                 pass
             if ele_conv == 5:
-                pass
+                menu_general()
             else:
                 exit
-    
-def menu_usuarios():
-    os.system('cls')
+
+def menu_prestamo():
+    print("***** Menú Préstamos *****")
     menu = {
-        1: "Alta Cliente",
-        2: "Consulta Cliente",
-        3: "Actualizar Cliente",
-        4: "Eliminar Cliente"
+        1: "Prestar Libro",
+        2: "Registrar Devolución",
+        3: "Volver al Menú Principal"
     }
     despliega_menu(menu)
-    eleccion = input("Ingrese su eleccion:")
-    eleccion = input("Ingrese su eleccion:")
+    eleccion = solicita_eleccion()
     if not valida_numero(eleccion):
         os.system('cls')
-        print("Eleccion errónea. Ingrese valor numérico correspondiente")
-        menu_usuarios()
+        imprime_respuesta('no_numero')
+        menu_prestamo()
     else:
         ele_conv = int(eleccion)
         if not valida_eleccion(menu,ele_conv):
-            print("Eleccion errónea. Ingrese valor correspondiente")
-            menu_usuarios()
+            imprime_respuesta('no_eleccion')
+            menu_prestamo()
         else:
-            print("Eleccion correcta {}".format(ele_conv))
+            #print("Eleccion correcta {}".format(ele_conv))
             if ele_conv == 1:
                 pass
             if ele_conv == 2:
                 pass
             if ele_conv == 3:
-                pass
-            if ele_conv == 4:
-                pass
-            else:
-                exit
-
-def menu_prestamo():
-    os.system('cls')
-    menu = {
-        1: "Prestar Libro",
-        2: "Registrar Devolución"
-    }
-    despliega_menu(menu)
-    eleccion = input("Ingrese su eleccion:")
-    if not valida_numero(eleccion):
-        os.system('cls')
-        print("Eleccion errónea. Ingrese valor numérico correspondiente")
-        menu_prestamo()
-    else:
-        ele_conv = int(eleccion)
-        if not valida_eleccion(menu,ele_conv):
-            print("Eleccion errónea. Ingrese valor correspondiente")
-            menu_prestamo()
-        else:
-            print("Eleccion correcta {}".format(ele_conv))
-            if ele_conv == 1:
-                pass
-            if ele_conv == 2:
-                pass
+                menu_general()
             else:
                 exit
 
